591df2486502064b91bb65ce0d598367701cb481
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-09-28 08:04:49 +0900

    feat : 중복 로그인 처리 로직 구현

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,12 @@ from dj_rest_auth.serializers import JWTSerializerWithExpiration, TokenSerialize
 from .serializers import CustomJWTSerializer
 from rest_framework_simplejwt.views import TokenViewBase
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-from django.http import JsonResponse  
+from django.http import JsonResponse
+# TOKEN
+from dj_rest_auth.models import get_token_model
+from dj_rest_auth.utils import jwt_encode
+from dj_rest_auth.app_settings import create_token
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken 
 
 class CustomLoginView(LoginView):
 
@@ -24,6 +29,38 @@ class CustomLoginView(LoginView):
         else:
             response_serializer = TokenSerializer
         return response_serializer
+
+  # 토큰 생성 로직
+  def login(self):
+        self.user = self.serializer.validated_data['user']
+        token_model = get_token_model()
+
+        if getattr(settings, 'REST_USE_JWT', False):
+            # 토큰 생성 => REFRESH 체크
+            # 이때 user_id의 가장 최근(latest) token을 outstanding/blacklistedtoken 테이블에서 확인
+            # => 가장 최근에 발급된 해당유저의 refresh 토큰이 blacklistedtoken에 존재하는지 확인 
+            print(OutstandingToken.objects.filter(user_id=self.user).latest('created_at').id)
+            latest_refresh_token = OutstandingToken.objects.filter(user_id=self.user).latest('created_at')
+            # print(BlacklistedToken.objects.get(token_id=latest_refresh_token_id)) # 없으면 에러 발생 -> 생성X
+            print(BlacklistedToken.objects.filter(token_id=latest_refresh_token.id)) # 없으면 빈 리스트 -> 생성O
+            # check : 로그아웃 된 적 없이 다시 로그인이 된다면... (이 경우 브라우저를 닫아도 자동적으로 로그아웃(blacklist에 저장)되는 로직 필요?) 
+            print(f"유저의 가장 최근 REFRESH TOKEN이 blacklist에 존재하는지 CHECK(정상 로그아웃됨): {BlacklistedToken.objects.filter(token_id=latest_refresh_token.id).exists()}")
+            if not BlacklistedToken.objects.filter(token_id=latest_refresh_token.id).exists():
+                BlacklistedToken.objects.create(token_id=latest_refresh_token.id)
+                self.access_token, self.refresh_token = "", ""
+                # data = {
+                #     'err_msg' : '이전에 로그아웃을 하지 않고 브라우저를 종료하여 로그아웃 처리되었습니다. 다시 로그인해주세요!'
+                # }
+                # return Response(data=data, status=status.HTTP_406_NOT_ACCEPTABLE)
+            else:
+                self.access_token, self.refresh_token = jwt_encode(self.user)
+            # self.access_token, self.refresh_token = jwt_encode(self.user)
+            
+        elif token_model:
+            self.token = create_token(token_model, self.user, self.serializer)
+
+        if getattr(settings, 'REST_SESSION_LOGIN', True):
+            self.process_login()
 
   def get_response(self):
         serializer_class = self.get_response_serializer()
@@ -42,6 +79,14 @@ class CustomLoginView(LoginView):
                 'access_token': self.access_token,
             }
             # print(data['user'].is_superuser)
+            
+            # 중복 로그인처리
+            if self.refresh_token == "":
+                data = {
+                    'err_msg' : '이전에 로그아웃을 하지 않고 브라우저를 종료하여 로그아웃 처리되었습니다. 다시 로그인해주세요!',
+                    'err_code' : 'NOT_ACCEPTABLE'
+                }
+                return Response(data=data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
             if not auth_httponly:
                 data['refresh_token'] = self.refresh_token
